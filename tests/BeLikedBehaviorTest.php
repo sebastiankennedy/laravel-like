a2a3b94e64362cf7d6416bf762dfec45841b7925
SebastianKennedy <sebastiankennedy@foxmail.com>
2020-06-09 21:44:38 +0800

    refactor: PHPUnit Optimize

---
+++ tests/BeLikedBehaviorTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SebastianKennedy\LaravelLike\Behaviors;
+
+use SebastianKennedy\LaravelLike\Tests\LaravelLikeTest;
+use SebastianKennedy\LaravelLike\Tests\Models\Book;
+use SebastianKennedy\LaravelLike\Tests\Models\Post;
+use SebastianKennedy\LaravelLike\Tests\Models\User;
+
+class BeLikedBehaviorTest extends LaravelLikeTest
+{
+    public function testIsLikedBy()
+    {
+        $user = User::create(['name' => 'User']);
+        $post = Post::create(['title' => 'Post']);
+        $book = Book::create(['title' => 'Book']);
+
+        $this->assertFalse($book->isLikedBy($user));
+        $this->assertFalse($post->isLikedBy($user));
+    }
+
+    public function testLikes()
+    {
+        $book = Book::create(['title' => 'Book']);
+        $user1 = User::create(['name' => 'User 1']);
+        $user2 = User::create(['name' => 'User 2']);
+        $user3 = User::create(['name' => 'User 3']);
+
+        $user1->like($book);
+        $user2->like($book);
+        $user3->like($book);
+        $this->assertSame(3, $book->likes()->count());
+
+        $user1->unlike($book);
+        $this->assertSame(2, $book->likes()->count());
+    }
+
+    public function testLikers()
+    {
+        $book = Book::create(['title' => 'Book']);
+        $user1 = User::create(['name' => 'User 1']);
+        $user2 = User::create(['name' => 'User 2']);
+        $user3 = User::create(['name' => 'User 3']);
+
+        $user1->like($book);
+        $user2->like($book);
+        $user3->like($book);
+
+        $this->assertSame(3, $book->likers()->count());
+
+        $user3->unlike($book);
+        $this->assertSame(2, $book->likers()->count());
+    }
+}
---
+++ tests/LikerBehaviorTest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace SebastianKennedy\LaravelLike\Tests;
+
+use Illuminate\Support\Facades\Event;
+use SebastianKennedy\LaravelLike\Events\LikedEvent;
+use SebastianKennedy\LaravelLike\Events\UnLikedEvent;
+use SebastianKennedy\LaravelLike\Tests\Models\Book;
+use SebastianKennedy\LaravelLike\Tests\Models\Post;
+use SebastianKennedy\LaravelLike\Tests\Models\User;
+
+class LikerBehaviorTest extends LaravelLikeTest
+{
+    public function testLike()
+    {
+        $user = User::create(['name' => 'User']);
+        $book = Book::create(['title' => 'Book']);
+        $this->assertIsObject($user->like($book));
+
+        Event::assertDispatched(
+            LikedEvent::class,
+            function ($event) use ($user, $book) {
+                return $event->like->{config('like.morph_many_name')} instanceof Book
+                    && $event->like->{config('like.morph_many_name')}->id === $book->id
+                    && $event->like->user instanceof User
+                    && $event->like->user->id === $user->id;
+            }
+        );
+
+        $this->assertNull($user->like($book));
+    }
+
+    public function testUnlike()
+    {
+        $user = User::create(['name' => 'User']);
+        $book = Book::create(['title' => 'Book']);
+        $this->assertNull($user->unlike($book));
+
+        $this->assertIsObject($user->like($book));
+        $this->assertTrue($user->unlike($book));
+        Event::assertDispatched(
+            UnLikedEvent::class,
+            function ($event) use ($user, $book) {
+                return $event->like->{config('like.morph_many_name')} instanceof Book
+                    && $event->like->{config('like.morph_many_name')}->id === $book->id
+                    && $event->like->user instanceof User
+                    && $event->like->user->id === $user->id;
+            }
+        );
+    }
+
+    public function testToggleLike()
+    {
+        $user = User::create(['name' => 'User']);
+        $book = Book::create(['title' => 'Book']);
+        $this->assertIsObject($user->toggleLike($book));
+        $this->assertTrue($user->toggleLike($book));
+    }
+
+    public function testHasLiked()
+    {
+        $user = User::create(['name' => 'User']);
+        $book = Book::create(['title' => 'Book']);
+        $post = Post::create(['title' => 'Post']);
+
+        $user->like($book);
+        $this->assertTrue($user->hasLiked($book));
+        $this->assertFalse($user->hasLiked($post));
+    }
+
+    public function testLikes()
+    {
+        $user = User::create(['name' => 'User']);
+        $book = Book::create(['title' => 'Book']);
+        $post = Post::create(['title' => 'Post']);
+        $user->like($book);
+        $user->like($post);
+
+        $this->assertSame(2, $user->likes()->count());
+        $this->assertSame(1, $user->likes()->withType(Book::class)->count());
+        $this->assertSame(1, $user->likes()->withType(Post::class)->count());
+    }
+}
